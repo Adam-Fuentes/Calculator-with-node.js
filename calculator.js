7b55541db326da266094bd411458171fd510ca0a
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-29 11:33:54 +0100

    add body-parser and index.html

--- calculator.js
+++ calculator.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({//se necesita esto para poder usar el body parser
+    extended: true
+}));
  
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){//con el body parser tenemos ahora el body con los post pasados anteriormente
+
+    var result = Number(req.body.num1) + Number(req.body.num2);
+
+    res.send("El resultado es: " + result);
 });
  
 app.listen(3000, function(){
-    console.log("Server started at port 3000");
+    console.log("Server started at port 3000.");
 });
